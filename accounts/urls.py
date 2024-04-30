299d9e182109b45c097d750d5cb9484f3a68a98b
Juunsik <Juunsik29@gmail.com>
2024-04-30 14:34:37 +0900

    Add: signup, login
    - User model custom
    - User serializer
    - signup, login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from . import views
 
 urlpatterns = [
-    
+    path('signup/', views.SignupAPIView.as_view()),
+    path("login/", views.LoginAPIView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import update_last_login
+from .models import User
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class SignupAPIView(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response(
+                {"message": "아이디 또는 비밀번호가 일치하지 않습니다."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        refresh = RefreshToken.for_user(user)
+        update_last_login(None, user)
+
+        return Response(
+            {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=status.HTTP_200_OK,
+        )
