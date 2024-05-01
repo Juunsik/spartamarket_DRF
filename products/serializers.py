afb5ce1d75e667386d8fa9f04e8772f05e5342b6
Juunsik <Juunsik29@gmail.com>
2024-05-01 11:33:13 +0900

    Update: image 처리
    - image가 제대로 처리되지 않던 것을 해결

--- products/serializers.py
+++ products/serializers.py
@@ -4,12 +4,11 @@ from accounts.serializers import UserSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-
     author = serializers.SerializerMethodField()
     
     def get_author(self, obj):
         return obj.author.username
-
+    
     class Meta:
         model = Product
         fields = "__all__"
